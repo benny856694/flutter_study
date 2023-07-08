d794accce0335832770152fba73b31571a800ae2
Bin Shen <shenbinsc@qq.com>
2023-07-08 13:11:04 +0800

    just created

---
+++ lib/mobilescanner.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class Scanner extends StatelessWidget {
+  const Scanner({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mobile Scanner')),
+      body: MobileScanner(
+        // fit: BoxFit.contain,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+          for (final barcode in barcodes) {
+            debugPrint('Barcode found! ${barcode.rawValue}');
+          }
+        },
+      ),
+    );
+  }
+}
