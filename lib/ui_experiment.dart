25cf889a0aa138cadc192377af951a4ad85de9a2
Benny Shen <shenbinsc@qq.com>
2023-11-19 23:30:16 +0800

    cosmetic

--- lib/ui_experiment.dart
+++ lib/ui_experiment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class UiExperiment extends HookConsumerWidget {
@@ -48,12 +49,65 @@ class UiExperiment extends HookConsumerWidget {
           //   ),
           // ]),
         ),
-        body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      'https://th.bing.com/th/id/R.6f744baeaded6299ceb6174ca1817de8?rik=9eyUw3B3WRq%2brg&pid=ImgRaw&r=0'))),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                    gradient:
+                        LinearGradient(colors: [Colors.green, Colors.yellow])),
+                child: const Text('Hello World'),
+              ),
+              const Spacer(),
+              Container(
+                height: 30,
+                width: 250,
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    32,
+                  ),
+                  border: Border.all(
+                    width: 1,
+                  ),
+                ),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    //contentPadding: EdgeInsets.symmetric(vertical: 0),
+                    icon: Icon(Icons.search),
+                    hintText: 'Searching...',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              const Gap(8),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_none,
+                ),
+              ),
+              const Gap(8),
+              SizedBox.square(
+                dimension: 32,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(32),
+                  child: Image.network(
+                    'https://th.bing.com/th/id/OIP.OH5utUfV4EFNQ7p9NnpfUAHaLH?w=122&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const Gap(8),
+              const Text('Benny'),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.arrow_drop_down),
+              ),
+            ],
+          ),
         ),
       ),
     );
