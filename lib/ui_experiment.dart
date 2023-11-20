202b9df71d676d9cb9bcf3316a05f4a5f413fa15
shen bin <shenbinsc@qq.com>
2023-11-20 16:24:40 +0800

    cosmetic

--- lib/ui_experiment.dart
+++ lib/ui_experiment.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 class UiExperiment extends HookConsumerWidget {
   const UiExperiment({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final tapped = useState(false);
     return DefaultTabController(
       length: 3,
       child: Scaffold(
@@ -51,61 +54,162 @@ class UiExperiment extends HookConsumerWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Row(
+          child: Column(
             children: [
-              Container(
-                decoration: const BoxDecoration(
-                    gradient:
-                        LinearGradient(colors: [Colors.green, Colors.yellow])),
-                child: const Text('Hello World'),
-              ),
-              const Spacer(),
-              Container(
-                height: 30,
-                width: 250,
-                margin: const EdgeInsets.all(8),
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(
-                    32,
+              Row(
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [Colors.green, Colors.yellow])),
+                    child: const Text('Hello World'),
                   ),
-                  border: Border.all(
-                    width: 1,
+                  const Spacer(),
+                  Container(
+                    //height: 30,
+                    width: 250,
+                    margin: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        26,
+                      ),
+                      border: Border.all(
+                        width: 1,
+                      ),
+                    ),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.all(8),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          size: 20,
+                        ),
+                        prefixIconConstraints:
+                            BoxConstraints(minHeight: 20, minWidth: 25),
+                        hintText: 'Searching...',
+                        border: InputBorder.none,
+                      ),
+                    ),
                   ),
-                ),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    //contentPadding: EdgeInsets.symmetric(vertical: 0),
-                    icon: Icon(Icons.search),
-                    hintText: 'Searching...',
-                    border: InputBorder.none,
+                  const Gap(8),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.notifications_none,
+                    ),
                   ),
-                ),
+                  const Gap(8),
+                  SizedBox.square(
+                    dimension: 32,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(32),
+                      child: Image.network(
+                        'https://th.bing.com/th/id/OIP.OH5utUfV4EFNQ7p9NnpfUAHaLH?w=122&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const Gap(8),
+                  const Text('Benny'),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_drop_down),
+                  ),
+                ],
               ),
-              const Gap(8),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.notifications_none,
-                ),
-              ),
-              const Gap(8),
               SizedBox.square(
-                dimension: 32,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(32),
-                  child: Image.network(
-                    'https://th.bing.com/th/id/OIP.OH5utUfV4EFNQ7p9NnpfUAHaLH?w=122&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7',
-                    fit: BoxFit.cover,
+                dimension: 50,
+                child: GestureDetector(
+                  onTapDown: (_) => tapped.value = true,
+                  onTapUp: (details) => tapped.value = false,
+                  child: Container(
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 3, color: Colors.white),
+                      shape: BoxShape.circle,
+                      color: Colors.amber,
+                      boxShadow: tapped.value ? null : kElevationToShadow[6],
+                    ),
+                    child: const Text(
+                      '70%',
+                      style: TextStyle(fontWeight: FontWeight.w800),
+                    ),
                   ),
                 ),
               ),
-              const Gap(8),
-              const Text('Benny'),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.arrow_drop_down),
-              ),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 150,
+                    width: 250,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 2, 69, 62),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: Row(
+                        children: [
+                          const Expanded(
+                            flex: 4,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Sales',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 16),
+                                ),
+                                Text(
+                                  'sales statistics',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  'sales statistics',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white38,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: SleekCircularSlider(
+                              appearance: CircularSliderAppearance(
+                                  startAngle: -90,
+                                  angleRange: 360,
+                                  customWidths: CustomSliderWidths(
+                                    progressBarWidth: 10,
+                                    trackWidth: 10,
+                                  )),
+                              min: 0,
+                              max: 100,
+                              initialValue: 80,
+                              innerWidget: (percentage) => Center(
+                                  child: Text(
+                                '$percentage',
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
