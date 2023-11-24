495cdec1ef79508e2340bdeddd552a945848ac2d
shen bin <shenbinsc@qq.com>
2023-11-24 17:42:37 +0800

    perfect search box

--- lib/widget/search_box.dart
+++ lib/widget/search_box.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const borderColor = Color.fromARGB(255, 204, 204, 204);
+const searchIconBg = Color.fromARGB(255, 248, 248, 248);
+const borderRadius = 26.0;
+
 class SearchBox extends StatelessWidget {
   const SearchBox({
     super.key,
@@ -9,68 +13,54 @@ class SearchBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 40,
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(1),
-              clipBehavior: Clip.antiAlias,
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 198, 198, 198),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(24),
-                    bottomLeft: Radius.circular(24)),
-              ),
-              child: Container(
-                clipBehavior: Clip.antiAlias,
-                decoration: const BoxDecoration(
-                  //color: Color.fromARGB(255, 34, 34, 34),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(23),
-                      bottomLeft: Radius.circular(23)),
-                ),
-                child: const TextField(
-                  //style: TextStyle(color: Colors.white60),
-                  decoration: InputDecoration(
-                    //isDense: true,
-                    fillColor: Colors.white,
-                    filled: true,
-                    contentPadding: EdgeInsets.only(
-                      left: 18,
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        // padding: const EdgeInsets.only(
+        //   left: 16,
+        // ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(borderRadius),
+            color: Colors.white,
+            border: Border.all(color: borderColor)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(borderRadius - 1),
+          child: Row(
+            children: [
+              const Expanded(
+                child: Center(
+                  child: TextField(
+                    //style: TextStyle(color: Colors.white60),
+                    decoration: InputDecoration(
+                      isDense: true,
+                      fillColor: Colors.white,
+                      hoverColor: Colors.white,
+                      filled: true,
+                      contentPadding: EdgeInsets.only(
+                        left: 18,
+                      ),
+                      border: InputBorder.none,
+                      hintText: 'Search',
+                      //hintStyle: TextStyle(color: Colors.white60)),
                     ),
-                    border: InputBorder.none,
-                    hintText: 'Search',
-                    //hintStyle: TextStyle(color: Colors.white60)),
                   ),
                 ),
               ),
-            ),
-          ),
-          Container(
-            width: 80,
-            padding: const EdgeInsets.only(top: 1, right: 1, bottom: 1),
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 198, 198, 198),
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(24),
-                  bottomRight: Radius.circular(24)),
-            ),
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 240, 240, 240),
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(24),
-                    bottomRight: Radius.circular(24)),
-              ),
-              child: const Center(
-                child: Icon(
-                  Icons.search,
-                  size: 28,
+              Container(
+                width: 64,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                    color: searchIconBg,
+                    border: Border(left: BorderSide(color: borderColor))),
+                child: const Center(
+                  child: Icon(
+                    Icons.search,
+                    size: 28,
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
