b5e95ce8dc86c4733436f3749b7a11d5b8b59326
shen bin <shenbinsc@qq.com>
2023-12-01 14:22:45 +0800

    try backdropfilter, animate

--- lib/ui_experiment.dart
+++ lib/ui_experiment.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_studdy/widget/search_box.dart';
 import 'package:flutter_studdy/widget/video_player.dart';
@@ -12,9 +15,12 @@ class UiExperiment extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final tapped = useState(false);
+    final blur = useState(5.0);
+    const w = 200.0;
     return DefaultTabController(
       length: 3,
       child: Scaffold(
+        backgroundColor: Colors.black38,
         appBar: AppBar(
           titleSpacing: 0,
           centerTitle: true,
@@ -35,19 +41,99 @@ class UiExperiment extends HookConsumerWidget {
             child: Stack(
               children: [
                 Container(
-                  width: 100,
-                  height: 200,
+                  margin: EdgeInsets.only(top: 0, left: 0),
+                  width: w,
+                  height: w,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.red),
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(w / 2),
                   ),
-                  child: Container(
-                    //alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
+                )
+                    .animate(
+                      onPlay: (controller) => controller.repeat(reverse: true),
+                    )
+                    .move(
+                      begin: Offset(0, 0),
+                      end: Offset(30, 30),
+                      duration: 3.seconds,
+                    )
+                    .scale(end: Offset(0.5, 0.5)),
+                Container(
+                  margin: EdgeInsets.only(top: 0, left: w / 2),
+                  width: w,
+                  height: w,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(w / 2),
+                  ),
+                )
+                    .animate(
+                      delay: 2.seconds,
+                      onPlay: (controller) => controller.repeat(reverse: true),
+                    )
+                    .move(
+                      begin: Offset(0, 0),
+                      end: Offset(-30, 30),
+                      duration: 3.seconds,
+                    )
+                    .scale(end: Offset(0.5, 0.5)),
+                Container(
+                  margin: EdgeInsets.only(top: w / 2, left: w / 4),
+                  width: w,
+                  height: w,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(w / 2),
+                  ),
+                )
+                    .animate(
+                      delay: 4.seconds,
+                      onPlay: (controller) => controller.repeat(reverse: true),
+                    )
+                    .move(
+                      begin: Offset(0, 0),
+                      end: Offset(0, 30),
+                      duration: 3.seconds,
+                    )
+                    .scale(end: Offset(0.5, 0.5)),
+                Positioned.fill(
+                  child: ClipRRect(
+                    //,
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                          sigmaX: blur.value, sigmaY: blur.value),
+                      child: Container(
+                        color: Colors.white.withOpacity(0.2),
+                      ),
                     ),
-                    child: const Text('abc'),
                   ),
                 ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.white),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      "See the world",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                    bottom: 4,
+                    left: 5,
+                    right: 5,
+                    child: Slider(
+                      divisions: 50,
+                      min: 0,
+                      max: 50,
+                      onChanged: (value) => blur.value = value,
+                      value: blur.value,
+                    ))
               ],
             ),
           ),
